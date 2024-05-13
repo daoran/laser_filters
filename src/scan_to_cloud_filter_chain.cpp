2be7a97d750a398b5b53f81f663c9c4f2c027bad
josegarcia <josegarcia@pal-robotics.com>
2024-05-13 13:00:12 +0200

    feat: components support

--- src/scan_to_cloud_filter_chain.cpp
+++ src/scan_to_cloud_filter_chain.cpp
@@ -143,3 +143,11 @@ ScanToCloudFilterChain::scanCallback(
 
   cloud_pub_->publish(filtered_cloud);
 }
+
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// Register the component with class_loader.
+// This acts as a sort of entry point, allowing the component to be discoverable when its library
+// is being loaded into a running process.
+RCLCPP_COMPONENTS_REGISTER_NODE(ScanToCloudFilterChain)
